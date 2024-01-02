633b0db3ac89cf8866db7a2ed0b0a32d08ea34aa
foxx <james.k@konnektive.com>
2024-01-02 13:01:13 -0500

    .

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,11 @@
-use gtk;
-use gtk::prelude::*;
-use gtk::{glib, Application, ApplicationWindow, Button};
-
-fn main() -> glib::ExitCode {
-    let application = Application::builder()
-        .application_id("net.koonts.WordCount")
-        .build();
-
-    application.connect_activate(|app| {
-        let window = ApplicationWindow::builder()
-            .application(app)
-            .title("Word Count")
-            .default_width(350)
-            .default_height(70)
-            .build();
-
-        let button = Button::with_label("Open File");
-        button.connect_clicked(|_| {
-            // load file
-            eprintln!("Clicked!");
-        });
-        window.set_child(Some(&button));
-
-        window.present();
-    });
-
-    application.run()
+slint::slint!{
+    export component HelloWorld {
+        Text {
+            text: "hello world";
+            color: green;
+        }
+    }
+}
+fn main() {
+    HelloWorld::new().unwrap().run().unwrap();
 }
