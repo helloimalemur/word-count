879d03990066bac30b4a69d62b0d240b5d27102a
jkoonts <jkoontsiii@gmail.com>
2024-01-03 21:08:36 -0500

    .

--- src/entities/wordcountapp.rs
+++ src/entities/wordcountapp.rs
@@ -1,7 +1,7 @@
 use crate::entities::wordcountfile::WordCountFile;
 use crate::ui::ui::WordCount;
 use chrono::Local;
-use slint::{ComponentHandle, LogicalSize, Model, SharedString, Window, WindowSize};
+use slint::{ComponentHandle, LogicalSize, Model, ModelRc, SharedString, Window, WindowSize};
 use std::fmt::format;
 use std::sync::{Arc, Mutex, MutexGuard};
 pub struct WordCountApp {
@@ -18,33 +18,11 @@ impl WordCountApp {
         word_count_window.on_open_file_pressed(move || {
             // word count handle
             let word_count_upgraded_weak_handle = word_count_window_weak_handle.upgrade().unwrap();
-            // file open counter
-            let mut counter_value = word_count_upgraded_weak_handle.get_counter();
-            // gui struct
-            let mut array = word_count_upgraded_weak_handle.get_list_of_structs();
+
+            open_file(&word_count_upgraded_weak_handle);
 
 
-
-            let mut current_row = word_count_upgraded_weak_handle
-                .get_list_of_structs()
-                .row_count();
-            current_row = counter_value as usize;
-            let text = format!("text: {}", Local::now().timestamp());
-            array.set_row_data(current_row, (SharedString::from(text),));
-            word_count_upgraded_weak_handle.set_list_of_structs(array);
-
-            println!("{}", current_row);
-
-
-
-            // increment counter
-            counter_value += 1;
-            if counter_value == 10 {
-                counter_value = 0;
-            }
-            word_count_upgraded_weak_handle.set_counter(counter_value);
         });
-        let ws = WindowSize::Logical(LogicalSize::new(200f32, 300f32));
 
         word_count_window.run().unwrap();
 
@@ -58,4 +36,29 @@ impl WordCountApp {
     fn get_files(&self) -> Vec<WordCountFile> {
         self.files.lock().unwrap().clone()
     }
+}
+
+
+pub fn open_file(word_count_upgraded_weak_handle: &WordCount) {
+    let mut counter_value= word_count_upgraded_weak_handle.get_counter();
+    let mut array = word_count_upgraded_weak_handle.get_list_of_structs();
+
+    let mut current_row = word_count_upgraded_weak_handle
+        .get_list_of_structs()
+        .row_count();
+    current_row = counter_value as usize;
+    let text = format!("text: {}", Local::now().timestamp());
+    array.set_row_data(current_row, (SharedString::from(text),));
+    word_count_upgraded_weak_handle.set_list_of_structs(array);
+
+    println!("{}", current_row);
+
+
+
+    // increment counter
+    counter_value += 1;
+    if counter_value == 10 {
+        counter_value = 0;
+    }
+    word_count_upgraded_weak_handle.set_counter(counter_value);
 }
