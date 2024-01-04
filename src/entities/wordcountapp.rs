d4eac8cd190888661640cc240e4b6225aaea755e
jkoonts <jkoontsiii@gmail.com>
2024-01-03 20:51:26 -0500

    .

--- src/entities/wordcountapp.rs
+++ src/entities/wordcountapp.rs
@@ -6,41 +6,43 @@ use std::fmt::format;
 use std::sync::{Arc, Mutex, MutexGuard};
 pub struct WordCountApp {
     pub files: Arc<Mutex<Vec<WordCountFile>>>,
+    pub word_count_window: Arc<Mutex<WordCount>>
 }
 
 impl WordCountApp {
     pub fn new() -> WordCountApp {
         let word_count_window = WordCount::new().unwrap();
+
+
         let word_count_window_weak_handle = word_count_window.as_weak();
         word_count_window.on_open_file_pressed(move || {
             // word count handle
             let word_count_upgraded_weak_handle = word_count_window_weak_handle.upgrade().unwrap();
-            // counter
-            let mut val = word_count_upgraded_weak_handle.get_counter();
-
+            // file open counter
+            let mut counter_value = word_count_upgraded_weak_handle.get_counter();
+            // gui struct
             let mut array = word_count_upgraded_weak_handle.get_list_of_structs();
+
+
 
             let mut current_row = word_count_upgraded_weak_handle
                 .get_list_of_structs()
                 .row_count();
-            current_row = val as usize;
+            current_row = counter_value as usize;
             let text = format!("text: {}", Local::now().timestamp());
             array.set_row_data(current_row, (SharedString::from(text),));
             word_count_upgraded_weak_handle.set_list_of_structs(array);
 
             println!("{}", current_row);
 
-            // for i in 0..10 {
-            //     let mut array = wc.get_list_of_structs();
-            //     array.set_row_data(i as usize, (SharedString::from("asf"), SharedString::from("asf")))
-            // }
+
 
             // increment counter
-            val += 1;
-            if val == 10 {
-                val = 0;
+            counter_value += 1;
+            if counter_value == 10 {
+                counter_value = 0;
             }
-            word_count_upgraded_weak_handle.set_counter(val);
+            word_count_upgraded_weak_handle.set_counter(counter_value);
         });
         let ws = WindowSize::Logical(LogicalSize::new(200f32, 300f32));
 
@@ -48,6 +50,7 @@ impl WordCountApp {
 
         WordCountApp {
             files: Arc::new(Mutex::new(vec![])),
+            word_count_window: Arc::new(Mutex::new(word_count_window.clone_strong()))
         }
     }
     fn add_file() {}
