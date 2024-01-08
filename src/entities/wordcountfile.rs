684975133f4c5fa58e2315c98dec0f85f931ac38
jkoonts <jkoontsiii@gmail.com>
2024-01-08 17:54:30 -0500

    .

--- src/entities/wordcountfile.rs
+++ src/entities/wordcountfile.rs
@@ -28,22 +28,4 @@ impl WordCountFile {
             full_file_contents: "".to_string(),
         }
     }
-    fn set_note() {}
-    fn set_deadline() {}
-    fn calculate() {
-        // calc word count, para count, char count, white space count
-        // let path = Path::new(file_path);
-        // let file = File::open(&path)?;
-        // let reader = BufReader::new(file);
-        // let mut total_size = 0;
-        // let mut word_count = 0;
-        // for line in reader.lines() {
-        //     let line = line?;
-        //     total_size += line.as_bytes().len();
-        //     word_count += line.split_whitespace().count();
-    }
-    fn update_modified() {
-        // if last modified is less than ?
-        todo!()
-    }
 }
