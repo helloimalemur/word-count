e2378fde2204034f7af726afc7c6b80110bbaa59
jkoonts <jkoontsiii@gmail.com>
2024-01-08 22:20:08 -0500

    counts

--- src/calculations/counts.rs
+++ src/calculations/counts.rs
@@ -6,8 +6,12 @@ pub fn get_char_count(input: String) -> i64 {
     words_count::count(input.replace('\n', "").replace('\r', "")).characters as i64
 }
 
-pub fn get_cjk_count(input: String) -> i64 {
-    input.split("\n\n").count() as i64
+pub fn get_paragraph_count(input: String) -> i64 {
+    input.split("\n\n\n\n").count() as i64
+}
+
+pub fn _get_line_count(input: String) -> i64 {
+    (input.split("\n\n").count()-1) as i64
 }
 
 pub fn get_ws_count(input: String) -> i64 {
@@ -19,7 +23,8 @@ pub fn get_ws_count(input: String) -> i64 {
 // }
 
 pub fn get_nth_top_used_word(input: String, nth: usize) -> (String, usize) {
-    let (nth_string, nth_count) = words_count::count_separately(input.to_lowercase().as_str())
+    let binding = input.to_lowercase();
+    let (nth_string, nth_count) = words_count::count_separately(binding.as_str())
         .into_iter()
         .nth(nth)
         .unwrap();
@@ -27,6 +32,7 @@ pub fn get_nth_top_used_word(input: String, nth: usize) -> (String, usize) {
 }
 
 pub fn get_unique_words(input: String) -> usize {
-    let result = words_count::count_separately(input.to_lowercase().as_str());
+    let binding = input.to_lowercase();
+    let result = words_count::count_separately(binding.as_str());
     result.len()
 }
